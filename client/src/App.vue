d59d62dd57d693c3d5b658577f51501408a6f77c
George Pantazes <georgep@wolfram.com>
2021-06-05 16:28:18 -0500

    Add Vue Typescript support
    Add Typescript to the Vue project.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Post msg="Welcome to Your Vue.js App" />
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script>
-import Post from "./components/Post.vue";
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: "App",
+@Options({
   components: {
-    Post
-  }
-};
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
