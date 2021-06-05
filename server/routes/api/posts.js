f6e75358306b52c03d70d2a81454e18bcacac1d5
George Pantazes <georgep@wolfram.com>
2021-06-05 15:14:47 -0500

    Get Posts api route
    Get Posts api route is now working.

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,14 +4,26 @@ import mongodb from 'mongodb';
 const router = Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-    res.send('hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 
 // Add Post
 
 // Delete Post
+
+
+async function loadPostCollection() {
+    const client = await mongodb.MongoClient.connect("mongodb://api-service-account:password@localhost:27017", {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+
+    return client.db('fullstack_vue_express').collection('posts');
+    
+}
 
 
 export default router;
